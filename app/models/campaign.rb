e9055a8998594bc89f886672cb1e9cdfedf71035
waldofouche <waldofouche1@gmail.com>
2026-07-13 05:37:08 +1000

    Add campaign models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
 class Campaign < ApplicationRecord
-  has_many :regions, dependent: :destroy
-  has_many :quests, through: :regions
+has_many :regions, dependent: :destroy
+has_many :campaign_travels, foreign_key: :from_campaign_id
 end
--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,17 +1,9 @@
-# frozen_string_literal: true
-
 class Quest < ApplicationRecord
-  belongs_to :region
+belongs_to :region
 
-  enum :quest_type, {
-    primary: 0,
-    secondary: 1,
-    profession: 2,
-    mission: 3,
-    mini_mission: 4,
-    hero: 5,
-    challenge: 6,
-    dungeon: 7,
-    repeatable: 8
-  }
+enum :quest_type, {
+  primary: 0,
+  secondary: 1,
+  profession: 2
+}
 end
