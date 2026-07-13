0be70ecece887c856078fad2124e9475442af4b1
waldofouche <waldofouche1@gmail.com>
2026-07-13 17:41:38 +1000

    lint

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -7,6 +7,6 @@ class CampaignsController < ApplicationController
 
   def show
     @campaign = Campaign.includes(regions: :quests).find(params[:id])
-    @regions = @campaign.regions.sort_by { |region| [region.position || Float::INFINITY, region.name.to_s] }
+    @regions = @campaign.regions.sort_by { |region| [ region.position || Float::INFINITY, region.name.to_s ] }
   end
 end
