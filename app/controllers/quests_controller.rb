39893e589ad2a39921390be52a36047dac6d194f
waldofouche <waldofouche1@gmail.com>
2026-07-13 17:29:43 +1000

    Add campaign filter to quest catalogue

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -2,6 +2,9 @@
 
 class QuestsController < ApplicationController
   def index
+    @campaigns = Campaign.order(:name)
+    @selected_campaign = @campaigns.find_by(id: params[:campaign_id]) if params[:campaign_id].present?
     @quests = Quest.includes(region: :campaign).order(:name)
+    @quests = @quests.where(region: @selected_campaign.regions) if @selected_campaign
   end
 end
